498b47aa19859f4420ce96811f130434f5401282
snakul <86.nakulsharma@gmail.com>
2017-10-24 00:36:59 -0400

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -11,15 +11,14 @@ class GTS(Strategy):
     bars- a dataframe of price bars for above symbol
     trend method- how to determine trend'''
 
-    def __init__(self, symbol, bars, signal_method, *args):
+    def __init__(self, symbol, bars, signal_method):
         self.symbol = symbol
         self.bars= bars
         self.signal_method = signal_method
-        self.func_args = args
 
-    def generate_signals(self):
+    def generate_signals(self, *args):
         '''returns the dataframe of symbols comtaining signals to go long, short or hold (1,-1,0)'''
-        signals = self.signal_method(*self.func_args)
+        signals = self.signal_method(args)
         # take the difference of the signals in order to generate actual trading orders
         return signals
 
